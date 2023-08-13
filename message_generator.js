fd7492cf279b0328ff83ff3904b72103d3606197
Terry Petersen <arigorn_15@yahoo.com>
2023-08-13 11:31:35 -0400

    Completed functionality to print out random message

--- message_generator.js
+++ message_generator.js
@@ -2,8 +2,10 @@ const adjectives = ['early', 'smelly', 'ugly', 'dead', 'happy'];
 const animals = ['bird', 'cow', 'spider', 'elephant', 'tardigrade'];
 const nouns = ['worm', 'shaft', 'cash', 'elixer of life', 'road-kill'];
 
-const selectRandomItem = arr => arr[Math.floor[Math.random * arr.length]];
+const selectRandomItem = arr => arr[Math.floor(Math.random() * arr.length)];
 
 const blank1 = selectRandomItem(adjectives);
 const blank2 = selectRandomItem(animals);
 const blank3 = selectRandomItem(nouns);
+
+console.log(`The ${blank1} ${blank2} gets the ${blank3}`);
